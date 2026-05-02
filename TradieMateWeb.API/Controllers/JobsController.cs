156ec7cd1107c51720dd27f4f5281c7f4d8cdc34
Tarique <simply.tarique@gmail.com>
2026-05-02 10:47:15 +0800

    Fixed invoice items in jobs query

--- TradieMateWeb.API/Controllers/JobsController.cs
+++ TradieMateWeb.API/Controllers/JobsController.cs
@@ -27,12 +27,13 @@ public class JobsController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-    var jobs = await _db.Jobs
-        .Where(j => j.UserId == GetUserId())
-        .Include(j => j.Client)
-        .Include(j => j.InvoiceItems)
-        .OrderByDescending(j => j.JobDate)
-        .ToListAsync();
+        var jobs = await _db.Jobs
+            .Where(j => j.UserId == GetUserId())
+            .Include(j => j.Client)
+            .Include(j => j.InvoiceItems)
+            .OrderByDescending(j => j.JobDate)
+            .ToListAsync();
+        return Ok(jobs);
     }
 
     [HttpPost]
